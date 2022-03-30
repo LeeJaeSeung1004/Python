c2a3de028eff58c8cde23b112f3772efbc4dde16
LeeJaeSeung1004 <dlwotmd281@naver.com>
2022-03-30 11:02:53 +0900

    2022-03-30

---
+++ 066_Quiz10.py
@@ -0,0 +1,15 @@
+# Quiz) 프로젝트 내에 나만의 시그니처를 남기는 모듈을 만드세요
+
+# 조건 : 모듈 파일명은 byme.py 로 작성
+
+# (모듈 사용 예제)
+# import byme
+# byme.sign()
+
+# (출력 예제)
+# 이프로그램은 이재승에 의해 만들어졌습니다
+# 이메일 : dlwotmd281@naver.com
+# github : https://github.com/LeeJaeSeung1004
+
+import byme
+byme.sign()
---
+++ byme.py
@@ -0,0 +1,4 @@
+def sign():
+    print("이프로그램은 이재승에 의해 만들어졌습니다")
+    print("이메일 : dlwotmd281@naver.com")
+    print("github : https://github.com/LeeJaeSeung1004")
